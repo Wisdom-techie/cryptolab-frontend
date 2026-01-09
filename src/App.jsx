dfad613886408ffe2c8c541521af4a9468c0a265
Wisdom-techie <wisdomstephen263@gmail.com>
2026-01-09 23:42:06 +0100

    Fix navbar import case sensitivity

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Navbar from "./components/navbar/Navbar";
+import Navbar from './components/navbar/navbar'
 import Footer from "./components/footer/Footer";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Auth from "./pages/Auth";
